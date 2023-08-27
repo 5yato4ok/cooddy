9d9674c05257b74dff47a1466662156cae46dd20
5yato4ok <13301101+5yato4ok@users.noreply.github.com>
2023-08-27 20:43:26 +0200

    Add crunched fix for support of lambdas in symbolic execution

--- include/ast/LambdaExpression.h
+++ include/ast/LambdaExpression.h
@@ -6,13 +6,19 @@
 #define COODDY_LAMBDAEXPRESSION_H
 
 #include "Node.h"
+#include "FunctionDecl.h"
 
 namespace HCXX {
 
 class LambdaExpression : public HCXX::CompoundNode {
 public:
+    LambdaExpression(Node *funcDecl) : m_operatorDecl(funcDecl) {}
     DECLARE_KIND(CompoundNode, Node::Kind::LAMBDA_EXPRESSION);
-    DECLARE_SERIALIZE(LambdaExpression, nullptr);
+    DECLARE_SERIALIZE(LambdaExpression, m_operatorDecl);
+
+    Node *GetOperatorDecl() const { return m_operatorDecl; }
+private:
+    Node* m_operatorDecl;
 };
 
 }  // namespace HCXX
